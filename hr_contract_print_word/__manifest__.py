74f799e2c3713d067b88e6a7263562dbedbf1b09
datht260197 <datht.tiendatit@gmail.com>
2025-11-01 20:58:59 +0700

    Update manifest with new summary and version

--- hr_contract_print_word/__manifest__.py
+++ hr_contract_print_word/__manifest__.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 {
     "name": "HR Contract Print Word",
-    "summary": "In hợp đồng lao động ra Word theo mẫu & biến Việt hóa",
-    "version": "18.0.1.0.0",
+    "summary": "In hợp đồng lao động ra Word với đầy đủ thông tin nhân viên",
+    "version": "18.0.1.1.0",
     "category": "Human Resources/Contracts",
     "license": "LGPL-3",
+    "author": "Việt Hàn Solutions",
     "depends": ["hr_contract"],
-    "data": ["views/hr_contract_views.xml"],
+    "data": [
+        "views/hr_contract_views.xml",
+    ],
     "external_dependencies": {"python": ["docxtpl"]},
     "installable": True,
-    "application": False
+    "application": False,
 }
